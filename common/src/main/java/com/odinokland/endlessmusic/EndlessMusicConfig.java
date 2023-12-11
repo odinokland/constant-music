7643906319a1657f385f01ea4597489022feee45
Odin Okland <odinokland@gmail.com>
2023-12-11 01:39:06 -0800

    feat: added option slider for delay configuration

--- common/src/main/java/com/odinokland/endlessmusic/EndlessMusicConfig.java
+++ common/src/main/java/com/odinokland/endlessmusic/EndlessMusicConfig.java
@@ -3,10 +3,12 @@ package com.odinokland.endlessmusic;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = EndlessMusic.MODID)
 public class EndlessMusicConfig implements ConfigData {
 
+    @ConfigEntry.BoundedDiscrete(min = 0, max = 200)
     public int timer = 0;
 
     public static EndlessMusicConfig getConfig() {
