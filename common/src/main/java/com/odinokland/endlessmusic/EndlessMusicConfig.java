9e5666ea6cf24d47afebaee21cd227cb8aadf073
Odin Okland <odinokland@gmail.com>
2023-12-12 10:47:40 -0800

    refactor: refactored code to ensure cloth config is a soft dependency and storing config in TOML is supported

--- common/src/main/java/com/odinokland/endlessmusic/EndlessMusicConfig.java
+++ common/src/main/java/com/odinokland/endlessmusic/EndlessMusicConfig.java
@@ -2,6 +2,7 @@ package com.odinokland.endlessmusic;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.ConfigHolder;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
@@ -13,5 +14,13 @@ public class EndlessMusicConfig implements ConfigData {
 
     public static EndlessMusicConfig getConfig() {
         return AutoConfig.getConfigHolder(EndlessMusicConfig.class).getConfig();
+    }
+
+    public static void setTimer(int value) {
+        ConfigHolder<EndlessMusicConfig> holder = AutoConfig.getConfigHolder(EndlessMusicConfig.class);
+        EndlessMusicConfig config = holder.getConfig();
+        config.timer = value;
+        holder.setConfig(config);
+        holder.save();
     }
 }
--- forge/src/main/java/com/odinokland/endlessmusic/forge/EndlessMusicImpl.java
+++ forge/src/main/java/com/odinokland/endlessmusic/forge/EndlessMusicImpl.java
@@ -1,5 +1,7 @@
 package com.odinokland.endlessmusic.forge;
 
+import com.moandjiezana.toml.Toml;
+import com.moandjiezana.toml.TomlWriter;
 import com.odinokland.endlessmusic.EndlessMusic;
 import com.odinokland.endlessmusic.EndlessMusicConfig;
 import me.shedaniel.autoconfig.AutoConfig;
@@ -12,6 +14,11 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.IExtensionPoint;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 @Mod(EndlessMusic.MODID)
 public class EndlessMusicImpl {
 
@@ -23,5 +30,21 @@ public class EndlessMusicImpl {
 
     public static boolean isClothConfigLoaded() {
         return ModList.get().isLoaded("cloth_config");
+    }
+
+    public static int readValue() {
+        Toml toml = new Toml().read(new File(EndlessMusic.configFile.toString()));
+        return toml.getLong("timer").intValue();
+    }
+
+    public static void writeValue(int value) {
+        TomlWriter tomlWriter = new TomlWriter();
+        try {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("timer", value);
+            tomlWriter.write(map, new File(EndlessMusic.configFile.toString()));
+        } catch (IOException e) {
+            // throw new RuntimeException(e);
+        }
     }
 }
