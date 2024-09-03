1919ba336023df19e62ed34d4f39c8ed5dac15d7
Odin Okland <odinokland@gmail.com>
2024-09-02 20:08:47 -0700

    Get quilt working on 1.20.4

--- neoforge/src/main/java/com/odinokland/constantmusic/ClientHandler.java
+++ neoforge/src/main/java/com/odinokland/constantmusic/ClientHandler.java
@@ -1,15 +1,18 @@
 package com.odinokland.constantmusic;
 
+import com.odinokland.constantmusic.gui.ConstantMusicConfigScreen;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModLoadingContext;
-import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.client.ConfigScreenHandler;
 
-@EventBusSubscriber(modid = Constants.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = Constants.MOD_ID)
 public class ClientHandler {
     @SubscribeEvent
     public static void onFMLClientSetupEvent(FMLClientSetupEvent event) {
-        ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, ClientConfigHelper::new);
+        ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> {
+            return new ConfigScreenHandler.ConfigScreenFactory((minecraft, parent) -> new ConstantMusicConfigScreen(parent));
+        });
     }
 }
