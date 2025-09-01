1f7d545615cd7afbd9437c502ace7554e6f152bd
Odin Okland <odinokland@gmail.com>
2025-09-01 01:14:39 -0700

    Update the run matrix for the proper runtime test

--- .github/scripts/generate-run-matrix.sh
+++ .github/scripts/generate-run-matrix.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+declare -A test_runtime=([fabric]="fabric" [forge]="lexforge" [neoforge]="neoforge")
+
 matrix_content="{\"include\":["
 enabled_platforms=$(awk -F= '/stonecutter_enabled_platforms/{print $2}' gradle.properties | tr -d ' ')
 
 for platform in $(echo $enabled_platforms | tr ',' ' '); do
   versions=$(awk -F= '/stonecutter_enabled_'$platform'_versions/{print $2}' gradle.properties | tr -d ' ')
   for version in $(echo $versions | tr ',' ' '); do
-    matrix_entry="{\"mod_loader\":\"$platform\",\"version\":\"$version\"},"
+    matrix_entry="{\"mod_loader\":\"$platform\",\"test_runtime\":\"${test_runtime[${platform}]}\",\"version\":\"$version\"},"
     matrix_content+="$matrix_entry"
   done
 done
